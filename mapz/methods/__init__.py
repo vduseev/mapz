74776b5a391d0cbb1aad8893a28148361f605839
vduseev <vagiz@duseev.com>
2020-11-02 21:45:39 +0100

    ♻️ Relocate methods package

--- mapz/methods/__init__.py
+++ mapz/methods/__init__.py
@@ -1,12 +1,25 @@
-from .getsert import getsert
-from .get import get
-from .traverse import traverse
-from .update import update
+from .get import get, getsert
+from .traverse import traverse, TraverseModificatorCallable, OrderingCallable
+from .overwrite import overwrite
+from .clone import clone, deepclone
+from .splitkey import splitkey, SplitkeyModificatorCallable
+from .put import put
+from .merge import merge
+from .apply import apply
 
 
 __all__ = [
     getsert.__name__,
     get.__name__,
     traverse.__name__,
-    update.__name__,
+    overwrite.__name__,
+    clone.__name__,
+    deepclone.__name__,
+    splitkey.__name__,
+    put.__name__,
+    merge.__name__,
+    apply.__name__,
+    "TraverseModificatorCallable",
+    "OrderingCallable",
+    "SplitkeyModificatorCallable",
 ]
