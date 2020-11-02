e77ca0ffcf7cc391bffe32e23acc44db037567e4
vduseev <vagiz@duseev.com>
2020-11-02 21:45:54 +0100

    ♻️ Relocate modifiers package

---
+++ mapz/modifiers/__init__.py
@@ -0,0 +1,11 @@
+from .case import to_lowercase, to_uppercase
+from .flatten import to_flat
+from .table import to_table
+
+
+__all__ = [
+    to_lowercase.__name__,
+    to_uppercase.__name__,
+    to_flat.__name__,
+    to_table.__name__,
+]
