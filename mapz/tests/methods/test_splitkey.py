a9fb6d80521cea14fdb2070e251aea38b616d374
vduseev <vagiz@duseev.com>
2020-12-13 19:01:04 +0100

    🐛 Fixed flake8 errors in splitkey tests

--- mapz/tests/methods/test_splitkey.py
+++ mapz/tests/methods/test_splitkey.py
@@ -1,7 +1,13 @@
+"""Test key-splitting function."""
+
 from mapz.methods.splitkey import splitkey
 
 
 def test_non_str():
+    """Test splitting non-string objects.
+
+    Splitting should not occur.
+    """
 
     assert splitkey(True) == [True]
 
@@ -9,11 +15,16 @@ def test_non_str():
 
 
 def test_str_noprefix_nosep_nomod():
+    """Split strings without prefix and with default separator."""
 
+    # No separator in value
     assert splitkey("name") == ["name"]
 
+    # This value contains the separator
     assert splitkey("AWS_DEFAULT_REGION") == ["AWS", "DEFAULT", "REGION"]
 
+    # No matter how many separators in the value there are
+    # they will be accurately cut off.
     assert splitkey("my__name") == ["my", "name"]
 
     assert splitkey("__my_name__") == ["my", "name"]
@@ -24,6 +35,7 @@ def test_str_noprefix_nosep_nomod():
 
 
 def test_str_prefix_nosep_nomod():
+    """Split string based on given prefix and default separator."""
 
     assert splitkey("AWS_DEFAULT_REGION", prefix="AWS_") == [
         "DEFAULT",
@@ -38,6 +50,7 @@ def test_str_prefix_nosep_nomod():
 
 
 def test_str_prefix_sep_nomod():
+    """Split string based on given prefix and separator."""
 
     assert splitkey("AWS_DEFAULT_REGION", prefix="AWS", sep="_") == [
         "DEFAULT",
@@ -54,6 +67,12 @@ def test_str_prefix_sep_nomod():
 
 
 def test_str_prefix_sep_mod():
+    """Split string using given prefix, separator, and modificator.
+
+    Modificator function is applied to split parts before returning
+    the results.
+    """
+
     def _modif(key, parts):
         return [p.lower() for p in parts]
 
