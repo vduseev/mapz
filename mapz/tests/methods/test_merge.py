93f690341e3d9fbd66aceff52e34c55783154881
vduseev <vagiz@duseev.com>
2020-12-28 16:09:04 +0100

    🚨 Fix flake8 errors for merge tests

--- mapz/tests/methods/test_merge.py
+++ mapz/tests/methods/test_merge.py
@@ -1,3 +1,5 @@
+"""Test merge method."""
+
 from mapz.methods.merge import merge
 
 import pytest
@@ -5,17 +7,33 @@ import pytest
 
 @pytest.fixture
 def data():
-    return {"name": "Boris", "data": {"songs": ["Du Hast", "Du Hast - Live"]}}
+    """Provide common dict structure for tests below."""
+
+    return {
+        "name": "Boris",
+        "music": {"songs": ["Du Hast", "Du Hast - Live"]},
+    }
 
 
 @pytest.fixture
-def mergein():
+def data_to_merge_in():
+    """Provide common structure to be merged in to the initial dict."""
+
     return {"name": "Duhast", "parental": "Vyacheslavovich"}
 
 
-def test_merge(data, mergein):
+def test_merge(data, data_to_merge_in):
+    """Test that merge properly overrides the fields.
 
-    merged = merge(data, mergein)
+    The 'name' should overwriten entirely. And a new field 'parental'
+    gets to be added to the final merged structure.
+    """
 
+    merged = merge(data, data_to_merge_in)
+
+    # Check name is overwritten.
     assert merged["name"] == "Duhast"
+    # Check 'music' field is still there.
+    assert merged["music"]["songs"][1] == "Du Hast - Live"
+    # Check that new field has appeared.
     assert merged["parental"] == "Vyacheslavovich"
