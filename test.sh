deebed5d196bd1047b05b32fbdce64a699015e96
vduseev <vagiz@duseev.com>
2020-12-28 16:05:47 +0100

    📝  List all test methods for reference

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-poetry run pytest --cov . --cov-report html --flake8 --mypy --black
+
+# Run all tests
+# poetry run pytest --cov . --cov-report html --flake8 --mypy --black
+
+# Test Coverage
+poetry run pytest --cov . --cov-report html
+
+# MyPy
+poetry run mypy mapz --strict
+
+# Flake8
+poetry run pytest --flake8
+
+
