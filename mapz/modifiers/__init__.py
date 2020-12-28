47502a55ac4c37891d503448c5209e596dbe89c4
vduseev <vagiz@duseev.com>
2020-12-28 17:20:01 +0100

    🚨 Fixed flake8 errors for modifiers module

--- mapz/modifiers/__init__.py
+++ mapz/modifiers/__init__.py
@@ -1,18 +1,18 @@
-from .case import to_lowercase as to_lowercase, to_uppercase as to_uppercase
-from .flatten import to_flat as to_flat
-from .table import (
-    to_table as to_table,
-    RowType as RowType,
-    HeaderType as HeaderType,
-    TableType as TableType,
+"""Modifiers transform given mapping structures into something new.
+
+For example, transform a dictionary into a printable table, flat structure,
+plain dict object, or a dictionary with different keys.
+"""
+
+from .case import (  # noqa: F401
+    to_lowercase as to_lowercase,
+    to_uppercase as to_uppercase,
 )
-from .mapping import to_dict as to_dict
-
-
-# __all__ = [
-#     to_lowercase.__name__,
-#     to_uppercase.__name__,
-#     to_flat.__name__,
-#     to_table.__name__,
-#     to_dict.__name__,
-# ]
+from .flatten import to_flat as to_flat  # noqa: F401
+from .mapping import to_dict as to_dict  # noqa: F401
+from .table import (  # noqa: F401
+    HeaderType as HeaderType,
+    RowType as RowType,
+    TableType as TableType,
+    to_table as to_table,
+)
